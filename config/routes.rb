2dd8e4c0bf93ff016b0689cae41a3c1fc583c1ff
Yuliya Pelekh <jpelekh@mail.ru>
2015-10-09 11:59:47 +0300

    Some styles added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get '/contacts' => 'static_pages#contact', as: :contacts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
